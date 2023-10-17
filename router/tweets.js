0047e1d457a25ec1ef729d62717a09f35b84d158
yewon97 <dpdnjs9936@gmail.com>
2023-10-18 00:17:42 +0900

    validation 공부

--- router/tweets.js
+++ router/tweets.js
@@ -1,18 +1,33 @@
 import express from 'express';
+import { body } from 'express-validator';
 import 'express-async-errors';
 import * as tweetController from '../controller/tweet.js';
+import { validate } from '../middleware/validator.js';
+
+// validation
+// sanitization
+// Contract Testing: Client-Server
 
 const router = express.Router();
+
+const validateTweet = [
+  body('text')
+    .trim()
+    .isLength({ min: 3 })
+    .notEmpty()
+    .withMessage('내용을 입력해주세요(세글자 이상)'),
+  validate,
+];
 
 router
   .route('/') //
   .get(tweetController.getTweets)
-  .post(tweetController.createTweet);
+  .post(validateTweet, tweetController.createTweet);
 
 router
   .route('/:id') //
   .get(tweetController.getTweet)
-  .put(tweetController.updateTweet)
+  .put(validateTweet, tweetController.updateTweet)
   .delete(tweetController.deleteTweet);
 
 export default router;

309dd904e67b2e34881132381c1c4139f2bcb3bb
yewon97 <dpdnjs9936@gmail.com>
2023-10-24 00:19:25 +0900

    관리자 정보 db (mysql)에서 가져오기

--- data/auth.js
+++ data/auth.js
@@ -1,35 +1,27 @@
 import { v4 as uuid } from 'uuid';
-
-// test1234!
-let users = [
-  {
-    id: '1',
-    username: 'josh',
-    password: '$2b$12$iqLUWYvop25tb.rxyQT6muHFlA21Esbd.409A/2MIP8S/wLOHotzK',
-    name: 'Josh',
-    email: 'josh@google.com',
-    url: 'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?auto=format&fit=crop&q=80&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=3000',
-  },
-  {
-    id: '2',
-    username: 'yewon',
-    password: '$2b$12$iqLUWYvop25tb.rxyQT6muHFlA21Esbd.409A/2MIP8S/wLOHotzK',
-    name: 'JeonYewon',
-    email: 'yewon@google.com',
-  },
-];
+import { db } from '../db/database.js';
 
 export async function findByUsername(username) {
-  const found = users.find((user) => user.username === username);
-  return found;
-}
-
-export async function createUser(user) {
-  const created = { ...user, id: uuid() };
-  users.push(created);
-  return created.id;
+  return db
+    .execute('SELECT * FROM users WHERE username=?', [username])
+    .then((res) => res[0][0]);
 }
 
 export async function findById(id) {
-  return users.find((user) => user.id === id);
+  return db
+    .execute('SELECT * FROM users WHERE id=?', [id]) //
+    .then((res) => {
+      console.log(res);
+      return res[0][0];
+    });
+}
+
+export async function createUser(user) {
+  const { username, password, name, email, url } = user;
+  return db
+    .execute(
+      'INSERT INTO users (username, password, name, email, url) VALUES (?,?,?,?,?)',
+      [username, password, name, email, url],
+    )
+    .then((res) => res[0].insertId);
 }
